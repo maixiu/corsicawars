097763ee847e3664e0a838298d1671901908b104
m.tabuteau <m.tabuteau@2fe264fc-d950-0410-9995-1db559ea6a3f>
2008-08-12 10:02:32 +0000

    Clean up

---
+++ trunk/Corsica.Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Corsica.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = string.Empty;
+            lol l = new lol();
+            RefereeContractClient client = new RefereeContractClient(new InstanceContext(l));
+
+            try
+            {
+                client.Subscribe("Player1");
+                client.Unsubscribe();
+            }
+            catch { }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+
+    class lol : IRefereeContractCallback
+    {
+
+        #region IRefereeContractCallback Members
+
+        public void OnCardPlayed(string user)
+        {
+            Console.WriteLine("received oncardplaycallback.");
+        }
+
+        #endregion
+    }
+}
