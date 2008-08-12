08587ef52289aabd0f35bd4e23c0f3ad54d21610
m.tabuteau <m.tabuteau@2fe264fc-d950-0410-9995-1db559ea6a3f>
2008-08-12 10:04:10 +0000

    clean up

---
+++ Corsica.RefereeService/RefereeContract.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Corsica.Service
+{
+    [ServiceContract(
+        Namespace="CorsicaWars",
+        SessionMode=SessionMode.Required,
+        CallbackContract = typeof(IRefereeConcractCallback))]
+    interface IRefereeContract
+    {
+        [OperationContract(IsOneWay=true)]
+        void Subscribe(string playerName);
+
+        [OperationContract(IsInitiating=false, IsTerminating=true, IsOneWay=true)]
+        void Unsubscribe();
+
+        [OperationContract(IsInitiating = false)]
+        void PlayCard();
+    }
+
+    interface IRefereeConcractCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void OnCardPlayed(string user);
+    }
+}
---
+++ Corsica.RefereeService/RefereeService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+using Corsica.Game;
+
+namespace Corsica.Service
+{
+    [ServiceBehavior(
+        InstanceContextMode=InstanceContextMode.PerSession,
+        ConcurrencyMode=ConcurrencyMode.Single)]
+    public class RefereeService : IRefereeContract
+    {
+        private static Dictionary<string, IRefereeConcractCallback> clientList = new Dictionary<string, IRefereeConcractCallback>();
+        private string playerName = string.Empty;
+
+        #region IRefereeContract Members
+        
+        public void Subscribe(string playerName)
+        {
+            if (!clientList.ContainsKey(playerName))
+            {
+                this.playerName = playerName;
+                IRefereeConcractCallback callback = OperationContext.Current.GetCallbackChannel<IRefereeConcractCallback>();
+                RefereeService.clientList.Add(playerName, callback);
+
+                Console.WriteLine("Player subscribe: " + playerName);
+            }
+            else
+            {
+                throw new FaultException<CorsicaPlayerExists>(new CorsicaPlayerExists());
+            }
+        }
+
+        public void Unsubscribe()
+        {
+            clientList.Remove(this.playerName);
+            Console.WriteLine("Player unsubscribe: " + playerName);
+        }
+
+        public void PlayCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
